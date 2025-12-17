55a906fad615ddf2cc0399da9db1c21f1a1150a3
temitopeakin1 <temitopeakinmegha@gmail.com>
2025-12-17 17:27:35 +0100

    add dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
-import express, { Application } from "express";
+import express, { Application, Router } from "express";
+import dotenv from "dotenv";
+import { Request, Response } from "express"; // Import Request and Response from express module
+// import userRoutes from "routes/userRoutes";
+// import productRoutes from "./routes/productRoutes"; // Corrected import path
+// import userRoutes from "./routes/userRoutes";
+// import errorHandler from "./middleware/errorHandler";
+// import connectDb from "../config/dbConnection";
 
+dotenv.config();
+
+// connectDb();
 export const app: Application = express();
-const port: number = parseInt(process.env.PORT || '5000');
+const port: number = parseInt(process.env.PORT || "5000");
 
-app.get("/", req: Request, res: Response) => {
-    res.send("leggo, Server is up and running!");
-}
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is fine and ready boom");
+});
 
+// middleware function for error handling
+// app.use(errorHandler);
+
+// middleware for body-parser
+app.use(express.json())
+
+const router: Router = express.Router();
+// app.use("/api/users", userRoutes); 
+
+// server to listen to the specific port (5000)
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`server running on port ${port}`);
 });
---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,13 @@
+// import validateToken from "../middleware/validateTokenHandler";
+// import { registerUser, loginUser, currentUser } from "../controller/userController";
+import express, {Router } from "express";
+
+const router: Router = express.Router();
+
+// router.post("/register", registerUser);
+
+// router.post("/login", loginUser);
+
+// router.get("/current", validateToken, currentUser);
+
+// export default router;
