0d2d57f163e8976061665f0fdb51ea53cd950088
temitopeakin1 <temitopeakinmegha@gmail.com>
2025-12-29 20:44:02 +0100

    update

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,22 +1,47 @@
+// import { Pool } from "pg";
+// import dotenv from "dotenv"
+
+// dotenv.config()
+
+
+// console.log(Pool)
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   port: Number(process.env.DB_PORT),
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
+
+// // Optional: test connection
+// pool.on("connect", () => {
+//   console.log("PostgreSQL pool connected");
+// });
+
+// export default pool;
+
+
 import { Pool } from "pg";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
-
-
-console.log(Pool)
+dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  port: Number(process.env.DB_PORT),
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
-// Optional: test connection
 pool.on("connect", () => {
-  console.log("PostgreSQL pool connected");
+  console.log("PostgreSQL connected");
+});
+
+pool.on("error", (err) => {
+  console.error("Unexpected PG error", err);
+  process.exit(1);
 });
 
 export default pool;
+
